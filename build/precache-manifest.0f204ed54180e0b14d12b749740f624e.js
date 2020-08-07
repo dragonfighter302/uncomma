78c3676a36fe73976590d023d693243004070cab
dragonfighter302 <vladimir.yeremin30@gmail.com>
2020-08-07 04:30:11 +0300

    Add files

---
+++ build/precache-manifest.0f204ed54180e0b14d12b749740f624e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "362ce415ddc9ba12ab5a7c9b4a9c899f",
+    "url": "/uncomma/index.html"
+  },
+  {
+    "revision": "e930834ba541a3cc3f45",
+    "url": "/uncomma/static/css/main.e78ddfe8.chunk.css"
+  },
+  {
+    "revision": "4048eb47034f3eff1824",
+    "url": "/uncomma/static/js/2.7f8845dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/uncomma/static/js/2.7f8845dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e930834ba541a3cc3f45",
+    "url": "/uncomma/static/js/main.b937b1ec.chunk.js"
+  },
+  {
+    "revision": "6f1f5169451ef7a5ad3d",
+    "url": "/uncomma/static/js/runtime-main.a719cda6.js"
+  },
+  {
+    "revision": "a8b0babf185599729a58e131ba3b9238",
+    "url": "/uncomma/static/media/comma.a8b0babf.png"
+  }
+]);
